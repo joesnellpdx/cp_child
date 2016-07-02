2214994e0412a277f0c938685dafd528d8c797a7
Joe Snell <joesnellpdx@gmail.com>
2016-07-01 17:56:42 -0700

    add site nav

--- inc/functions-custom.php
+++ inc/functions-custom.php
@@ -21,3 +21,40 @@ function get_attachment_id_from_src($url) {
 	$attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM " . $prefix . "posts" . " WHERE guid='%s';", $img_source ));
 	return $attachment[0];
 }
+
+function register_multiple_menus() {
+
+	register_nav_menus( array(
+		'sites' => 'Sites Menu',
+	) );
+}
+add_action( 'init', 'register_multiple_menus' );
+
+/**
+ * Add custom li class to site navigation
+
+ *
+ * @return custom class
+ */
+function site_menu_li_classes( $classes , $item, $args, $depth ) {
+	if( $args->theme_location === 'sites' ) {
+		$classes[] = 'navigation__item';
+	}
+
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', 'site_menu_li_classes', 10, 4 );
+
+/**
+ * Add custom link class to navigation
+
+ *
+ * @return custom class
+ */
+function site_menu_link_classes( $atts, $item, $args ) {
+	if( $args->theme_location === 'sites' ) {
+		$atts['class'] = 'navigation__link';
+	}
+	return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'site_menu_link_classes', 10, 3 );
