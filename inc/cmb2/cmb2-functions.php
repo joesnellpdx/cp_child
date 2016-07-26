292b5dd62656d1fb364cc8a26d84053103a9c378
Joe Snell <joesnellpdx@gmail.com>
2016-07-26 12:15:31 -0700

    update cmb2 custom page meta boxes output

--- inc/cmb2/cmb2-functions.php
+++ inc/cmb2/cmb2-functions.php
@@ -15,8 +15,8 @@ function jspdx_register_pacorp_metabox( ) {
 	$meta_boxes = new_cmb2_box( array(
 		'id' => 'pacorp',
 		'title' => __( 'Page Options', 'cmb2' ),
-		'object_types'  => array( 'page', ), // Post type
-		'show_on'      => array( 'pulsair-corporate' ),
+		'object_types' => array( 'page' ), // post type
+		'show_on'      => array( 'key' => 'page-template', 'value' => 'page-templates/pulsair-corporate.php' ),
 		'context' => 'normal',
 		'priority' => 'high',
 		'show_names'    => true, // Show field names on the left
@@ -137,32 +137,32 @@ add_filter( 'cmb2_init', 'jspdx_register_client_item_metabox' );
  * @param array $meta_box
  * @return bool display metabox
  */
-function be_metabox_show_on_template( $display, $meta_box ) {
-	if ( ! isset( $meta_box['show_on']['key'], $meta_box['show_on']['value'] ) ) {
-		return $display;
-	}
-
-	if ( 'template' !== $meta_box['show_on']['key'] ) {
-		return $display;
-	}
-
-	$post_id = 0;
-
-	// If we're showing it based on ID, get the current ID
-	if ( isset( $_GET['post'] ) ) {
-		$post_id = $_GET['post'];
-	} elseif ( isset( $_POST['post_ID'] ) ) {
-		$post_id = $_POST['post_ID'];
-	}
-
-	if ( ! $post_id ) {
-		return false;
-	}
-
-	$template_name = get_page_template_slug( $post_id );
-	$template_name = ! empty( $template_name ) ? substr( $template_name, 0, -4 ) : '';
-
-	// See if there's a match
-	return in_array( $template_name, (array) $meta_box['show_on']['value'] );
-}
-add_filter( 'cmb2_show_on', 'be_metabox_show_on_template', 10, 2 );
+//function be_metabox_show_on_template( $display, $meta_box ) {
+//	if ( ! isset( $meta_box['show_on']['key'], $meta_box['show_on']['value'] ) ) {
+//		return $display;
+//	}
+//
+//	if ( 'template' !== $meta_box['show_on']['key'] ) {
+//		return $display;
+//	}
+//
+//	$post_id = 0;
+//
+//	// If we're showing it based on ID, get the current ID
+//	if ( isset( $_GET['post'] ) ) {
+//		$post_id = $_GET['post'];
+//	} elseif ( isset( $_POST['post_ID'] ) ) {
+//		$post_id = $_POST['post_ID'];
+//	}
+//
+//	if ( ! $post_id ) {
+//		return false;
+//	}
+//
+//	$template_name = get_page_template_slug( $post_id );
+//	$template_name = ! empty( $template_name ) ? substr( $template_name, 0, -4 ) : '';
+//
+//	// See if there's a match
+//	return in_array( $template_name, (array) $meta_box['show_on']['value'] );
+//}
+//add_filter( 'cmb2_show_on', 'be_metabox_show_on_template', 10, 2 );
